5ca3f49efe5535b1d9858e57244e63f0debd591c
Maxon <notmax2000@mail.ru>
2023-02-02 13:56:20 +0700

    added delete functionality

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ Vue.component('main-board', {
             <p id="errorp">{{error}}</p>
             <div id="blocks_div">
                 <div id="first_col" class="col">
-                    <p class="col_title">Создание задач</p>
+                    <p class="col_title">Создание задачу</p>
                     <point-card v-for="card in blockOneCards" :pointsAndTitle="card"></point-card>
                     <form>
                         <div id="upperFormDiv">
@@ -14,11 +14,15 @@ Vue.component('main-board', {
                                 <p>Название карточки:</p>
                                 <input type="text" placeholder="Название" v-model="cardTitle" maxlength="15">
                             </div>
-                            <p>Количество заметок в карточке:</p>
-                            <input type="number" max="5" min="3" v-model="numberOfOptions">
-                            <input type="submit" value="Создать пункты" @click.prevent="makeOptionsArr" maxlength="20">
+                            <div>
+                                <p>Задание:</p>
+                                <textarea name="name" cols="20" rows="3" v-model="cardTask" placeholder="Задание" maxlength="60"></textarea>
+                            </div>
+                            <div>
+                                <p>Дэдлайн:</p>
+                                <input type="date" v-model="deadline">
+                            </div>       
                         </div>
-                        <input v-for="i in options" :key="i" class="pointInput" type="text" placeholder="пункт">
                         <input type="submit" @click.prevent="createCard" value="Создать карточку">
                     </form>
                 </div>
@@ -42,38 +46,24 @@ Vue.component('main-board', {
     data(){
         return{
             error: '',
-            numberOfOptions: 5,
             cardTitle: '',
-            options: [],
-            points: [],
+            cardTask: '',
             blockOneCards: [],
             blockTwoCards: [],
             blockThreeCards: [],
+            blockForthCards: [],
+            blockFifthCards: [],
             renderComponent: true,
         }
     },
     methods: {
-        makeOptionsArr(){
-            this.points.splice(0,this.points.length)
-            this.options.splice(0,this.options.length);
-
-            for(let i = 0; i < this.numberOfOptions; i++){
-                this.options.push(i)
-            }
-        },
         createCard(){
             if(this.blockOneCards.length < 3){
-                this.options.splice(0,this.options.length)
-                this.points.splice(0,this.points.length)
-                let points = document.querySelectorAll('.pointInput')
-
-                for(let i = 0; i < points.length; i++){
-                    this.points.push({point: points[i].value ? points[i].value:'Задание ' +(i+1), checked: false})
-                }
-
-                let copy = this.points.concat()
-
-                this.blockOneCards.push({title: this.cardTitle ? this.cardTitle : 'Без названия', points: copy})
+                this.blockOneCards.push({title: this.cardTitle ? this.cardTitle : 'Без названия',
+                                        task: this.cardTask ? this.cardTask : 'Задание',
+                                        deadline: this.deadline,
+                                        createdAt: this.currentDate,
+                                        toDelete: false})
 
                 this.error = ''
 
@@ -101,56 +91,30 @@ Vue.component('main-board', {
         }
     },
     beforeMount(){
-        this.loadCards()
+
     },
     mounted(){
-        eventBus.$on('pointCrossed', cardsCheck => {
-
-            if(this.blockTwoCards.length < 5){
-                for(let i = 0; i < this.blockOneCards.length; i++){
-                    let numbOfChecked = 0
-
-                    for(let j = 0; j < this.blockOneCards[i].points.length; j++){
-                        if(this.blockOneCards[i].points[j].checked === true){
-                            numbOfChecked += 1
-                        }
+        eventBus.$on('cardDelete', ()=>{
+            for(let column in this.allCardsByColumns){
+                for(let card in column){
+                    if(this.allCardsByColumns[column][card].toDelete){
+                        this.allCardsByColumns[column].splice(card, 1)
                     }
-
-                    if(this.blockOneCards[i].points.length/2 <= numbOfChecked && this.blockOneCards[i].points.length !== numbOfChecked){
-                        this.blockTwoCards.push(this.blockOneCards[i])
-                        this.blockOneCards.splice(i, 1)
-                    }
-                }
-                this.error = ''
-            }else{
-                this.error = 'Во втором столбце не может быть больше 5 карточек'
-            }
-
-
-            for(let i = 0; i < this.blockTwoCards.length; i++){
-                let numbOfChecked = 0
-
-                for(let j = 0; j < this.blockTwoCards[i].points.length; j++){
-                    if(this.blockTwoCards[i].points[j].checked === true){
-                        numbOfChecked += 1
-                    }
-                }
-
-
-                if(this.blockTwoCards[i].points.length === numbOfChecked){
-                    this.blockThreeCards.push(this.blockTwoCards[i])
-                    this.blockTwoCards.splice(i, 1)
                 }
             }
-            let jsonData = JSON.stringify(this.allCardsByColumns)
-            localStorage.setItem('jsonData', jsonData)
 
         })
     },
     computed: {
         allCardsByColumns() {
-            return [this.blockOneCards, this.blockTwoCards, this.blockThreeCards]
+            return [this.blockOneCards, this.blockTwoCards, this.blockThreeCards, this.blockForthCards, this.blockFifthCards]
         },
+        deadline(){
+            return new Date(3600*24*1000*19400).toISOString().substring(0,10)
+        },
+        currentDate() {
+            return new Date().toISOString().substring(0,10)
+        }
     }
 })
 
@@ -158,24 +122,27 @@ Vue.component('point-card', {
     template: `
         <div id="card">
             <p id="cardTitle">{{pointsAndTitle.title}}</p>
-            <ul id="pointslist">
-                <div v-for="point in pointsAndTitle.points"
-                     @click="point.checked=true"
-                     @click="pointCrossed"
-                     :class="{TaskDone:point.checked}">
-                    <li>{{point.point}}</li>
-                    <hr>
-                </div>
-            </ul>
+            <hr>
+            <p>{{pointsAndTitle.task}}</p>
+            <hr>
+            <p class="cardDate">Создано: {{pointsAndTitle.createdAt}}</p>
+            <p class="cardDate">Дэдлайн: {{pointsAndTitle.deadline}}</p>
+            <div class="cardBtnsDiv">
+                <button id="change_button">Изменить</button>
+                <button id="delete_button" @click="pointsAndTitle.toDelete=true; cardDelete()">Удалить</button>
+            </div>
         </div>
     `,
     props:{
         pointsAndTitle: null
     },
     methods: {
-        pointCrossed(){
-            eventBus.$emit('pointCrossed')
+        cardDelete(){
+            eventBus.$emit('cardDelete')
         }
+    },
+    computed: {
+
     }
 })
 
