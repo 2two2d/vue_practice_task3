690b7b82f8e90ae1b96941f4c6957fb944d79bdc
Maxon <notmax2000@mail.ru>
2023-02-03 10:42:45 +0700

    added move buttons

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ Vue.component('main-board', {
             <p id="errorp">{{error}}</p>
             <div id="blocks_div">
                 <div id="first_col" class="col">
-                    <p class="col_title">Создание задачу</p>
+                    <p class="col_title">Создание задач</p>
                     <point-card v-for="card in blockOneCards" :pointsAndTitle="card"></point-card>
                     <form>
                         <div id="upperFormDiv">
@@ -27,17 +27,14 @@ Vue.component('main-board', {
                     </form>
                 </div>
                 <div id="second_col" class="col">
-                    <p class="col_title" style="font-size: 1.5em">Запланированные задачи</p>
+                    <p class="col_title" style="font-size: 1.5em">Задачи в работе</p>
                     <point-card v-for="card in blockTwoCards" :pointsAndTitle="card"></point-card>
                 </div>
                 <div id="third_col" class="col">
-                    <p class="col_title">Задачи в работе</p>
+                    <p class="col_title">Тестирование</p>
                     <point-card v-for="card in blockThreeCards" :pointsAndTitle="card"></point-card>
                 </div>
                 <div id="forth_col" class="col">
-                    <p class="col_title">Тестирование</p>
-                </div>
-                <div id="fifth_col" class="col">
                     <p class="col_title">Выполненные задачи</p>
                 </div>
             </div>
@@ -52,7 +49,6 @@ Vue.component('main-board', {
             blockTwoCards: [],
             blockThreeCards: [],
             blockForthCards: [],
-            blockFifthCards: [],
             renderComponent: true,
         }
     },
@@ -63,7 +59,13 @@ Vue.component('main-board', {
                                         task: this.cardTask ? this.cardTask : 'Задание',
                                         deadline: this.deadline,
                                         createdAt: this.currentDate,
-                                        toDelete: false})
+                                        updatedAt: null,
+                                        toDelete: false,
+                                        toChange: false,
+                                        toMoveBack: false,
+                                        toMoveForward: false,
+                                        btnBack: false,
+                                        btnForward: true})
 
                 this.error = ''
 
@@ -96,21 +98,20 @@ Vue.component('main-board', {
     mounted(){
         eventBus.$on('cardDelete', ()=>{
             for(let column in this.allCardsByColumns){
-                for(let card in column){
+                for(let card in column+1){
                     if(this.allCardsByColumns[column][card].toDelete){
                         this.allCardsByColumns[column].splice(card, 1)
                     }
                 }
             }
-
         })
     },
     computed: {
         allCardsByColumns() {
-            return [this.blockOneCards, this.blockTwoCards, this.blockThreeCards, this.blockForthCards, this.blockFifthCards]
+            return [this.blockOneCards, this.blockTwoCards, this.blockThreeCards, this.blockForthCards]
         },
         deadline(){
-            return new Date(3600*24*1000*19400).toISOString().substring(0,10)
+            return new Date(new Date().getTime() + 24 * 60 * 60 * 1000 * 7).toISOString().substring(0,10)
         },
         currentDate() {
             return new Date().toISOString().substring(0,10)
@@ -121,15 +122,29 @@ Vue.component('main-board', {
 Vue.component('point-card', {
     template: `
         <div id="card">
-            <p id="cardTitle">{{pointsAndTitle.title}}</p>
-            <hr>
-            <p>{{pointsAndTitle.task}}</p>
-            <hr>
-            <p class="cardDate">Создано: {{pointsAndTitle.createdAt}}</p>
-            <p class="cardDate">Дэдлайн: {{pointsAndTitle.deadline}}</p>
-            <div class="cardBtnsDiv">
-                <button id="change_button">Изменить</button>
-                <button id="delete_button" @click="pointsAndTitle.toDelete=true; cardDelete()">Удалить</button>
+            <div v-if="pointsAndTitle.toChange===false">
+                <button v-if="pointsAndTitle.btnBack" @click="moveForward()" class="moveBackBtn"><</button>
+                <button v-if="pointsAndTitle.btnForward" @click="moveBack()" class="moveForwardBtn">></button>
+                <p id="cardTitle">{{pointsAndTitle.title}}</p>
+                <hr>
+                <p>{{pointsAndTitle.task}}</p>
+                <hr>
+                <p class="cardDate">Создано: {{pointsAndTitle.createdAt}}</p>
+                <p class="cardDate">Дэдлайн: {{pointsAndTitle.deadline}}</p>
+                <p v-if="pointsAndTitle.updatedAt" class="cardDate">Обновлён: {{pointsAndTitle.updatedAt}}</p>
+                <div class="cardBtnsDiv">
+                    <button class="just_button" @click="pointsAndTitle.toChange=true">Изменить</button>
+                    <button class="danger_button" @click="pointsAndTitle.toDelete=true; cardDelete()">Удалить</button>
+                </div>
+            </div>
+            <div v-else>
+                <p>Название: <input type="text" v-model="reserveTitle"></p>
+                <p>Задание: <input type="text" v-model="reserveTask"></p>
+                <p>Дэдлайн: <input type="date" v-model="reserveDeadLine"></p>
+                <div class="cardBtnsDiv">
+                    <button class="just_button" @click="pointsAndTitle.toChange=false">Отмена</button>
+                    <button class="just_button" @click="cardUpdate();setUpdateMark();pointsAndTitle.toChange=false">Изменить</button>
+                </div>
             </div>
         </div>
     `,
@@ -139,10 +154,28 @@ Vue.component('point-card', {
     methods: {
         cardDelete(){
             eventBus.$emit('cardDelete')
+        },
+        cardUpdate(){
+            this.pointsAndTitle.title = this.reserveTitle
+            this.pointsAndTitle.task = this.reserveTask
+            this.pointsAndTitle.deadline = this.reserveDeadLine
+        },
+        setUpdateMark(){
+            this.pointsAndTitle.updatedAt = new Date().toISOString().substring(0,10) + ' ' +  new Date().toISOString().substring(11,19)
+        },
+        moveForward(){
+            eventBus.$emit('cardMoveForward')
+        },
+        moveBack(){
+            eventBus.$emit('cardMoveBack')
         }
     },
-    computed: {
-
+    data() {
+        return{
+            reserveTitle: JSON.parse(JSON.stringify(this.pointsAndTitle.title)),
+            reserveTask: JSON.parse(JSON.stringify(this.pointsAndTitle.task)),
+            reserveDeadLine: JSON.parse(JSON.stringify(this.pointsAndTitle.deadline))
+        };
     }
 })
 
